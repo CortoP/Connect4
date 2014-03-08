ba9d6f030843e3036eebd82b8925f86f0a5fa725
CortoP <vlehuger@student.42.fr>
2014-03-08 15:36:51 +0100

    valid OK

--- srcs/test_win.c
+++ srcs/test_win.c
@@ -6,7 +6,7 @@
 /*   By: vlehuger <vlehuger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/08 11:19:16 by vlehuger          #+#    #+#             */
-/*   Updated: 2014/03/08 14:39:12 by vlehuger         ###   ########.fr       */
+/*   Updated: 2014/03/08 15:36:10 by vlehuger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,10 +46,7 @@ char	test_up_down(char **grid, int l, int c, char tok)
 
 	save = 0;
 	count = 0;
-	if (l >= 3)
-		i = l - 3;
-	else
-		i = 0;
+	i = l;
 	while (grid[i] && i <= l + 3)
 	{
 		if (grid[i][c] == tok)
@@ -69,28 +66,21 @@ char	test_diag_up_left(char **grid, int l, int c, char tok)
 	int		i;
 	int		j;
 	int		count;
-	int		save;
+	int		ret;
 
-	save = 0;
-	count = 0;
-	if (l >= 3)
-		i = l - 3 - 1;
-	else
-		i = -1;
-	if (c >= 3)
-		j = c - 3 - 1;
-	else
-		j = -1;
-	while (grid[++i] && grid[i][++j] && i <= l + 3)
-	{
-		if (grid[i][j] == tok)
-			count++;
-		if (count == 4)
-			return (1);
-		if (count == save)
-			count = 0;
-		save = count;
-	}
+	i = l;
+	j = c;
+	ret = 1;
+	count = 1;
+	while (grid[--i] && grid[i][--j] && grid[i][j] == tok && count < 3)
+		ret++;
+	i = l;
+	j = c;
+	count = 1;
+	while (grid[++i] && grid[i][++j] && grid[i][j] == tok && count < 3)
+		ret++;
+	if (ret == 4)
+		return (1);
 	return (0);
 }
 
@@ -104,24 +94,15 @@ char		test_diag_up_right(char **grid, int l, int c, char tok)
 
 	i = l;
 	j = c;
-	count = 0;
 	ret = 1;
-	while (grid[--i] && grid[i][++j] && count < 3)
-	{
-		if (grid[i][j] == tok)
-			ret++;
-	}
-	if (ret == 4)
-		return (1);
+	count = 1;
+	while (grid[--i] && grid[i][++j] && grid[i][j] == tok && count < 3)
+		ret++;
 	i = l;
 	j = c;
-	count = 0;
-	ret = 1;
-	while (grid[++i] && grid[i][--j] && count < 3)
-	{
-		if (grid[i][j] == tok)
-			ret++;
-	}
+	count = 1;
+	while (grid[++i] && grid[i][--j] && grid[i][j] == tok && count < 3)
+		ret++;
 	if (ret == 4)
 		return (1);
 	return (0);
