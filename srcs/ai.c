31233e6790199e8bbefb1c807f914fe2f8dc811a
CortoP <vlehuger@student.42.fr>
2014-03-09 11:58:54 +0100

    AI medium

--- srcs/ai.c
+++ srcs/ai.c
@@ -6,13 +6,76 @@
 /*   By: vlehuger <vlehuger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/08 17:27:19 by vlehuger          #+#    #+#             */
-/*   Updated: 2014/03/09 11:36:16 by vlehuger         ###   ########.fr       */
+/*   Updated: 2014/03/09 11:49:20 by vlehuger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <puissance_4.h>
 #include <stdlib.h>
 #include <libft.h>
+
+char		test_adv2(char **grid)
+{
+	int		line;
+	int		ret;
+	int		col;
+	int		tmp;
+
+	col = 0;
+	tmp = 10;
+	while (grid[0][col])
+	{
+		if (test_up(grid, col))
+		{
+			line = 0;
+			while (grid[line] && grid[line][col] == '.')
+				line++;
+			grid[line - 1][col] = 'X';
+			if (test_win(grid, col) == 1)
+				ret = 1;
+			else
+				ret = 8;
+			grid[line - 1][col] = '.';
+		}
+		col++;
+		if (ret < tmp)
+			tmp = ret;
+	}
+	return (tmp);
+}
+
+char		test_ai(char **grid)
+{
+	int		line;
+	int		ret;
+	int		col;
+	int		tmp;
+
+	col = 0;
+	tmp = 10;
+	while (grid[0][col])
+	{
+		if (test_up(grid, col))
+		{
+			line = 0;
+			while (grid[line] && grid[line][col] == '.')
+				line++;
+			grid[line - 1][col] = 'O';
+			if (test_win(grid, col) == 1)
+			{
+				grid[line - 1][col] = '.';
+				return (9);
+			}
+			else
+				ret = test_adv2(grid);
+			grid[line - 1][col] = '.';
+		}
+		col++;
+		if (ret < tmp)
+			tmp = ret;
+	}
+	return (tmp);
+}
 
 char		test_adv(char **grid)
 {
@@ -22,7 +85,7 @@ char		test_adv(char **grid)
 	int		tmp;
 
 	col = 0;
-	tmp = 1;
+	tmp = 10;
 	while (grid[0][col])
 	{
 		if (test_up(grid, col))
@@ -34,7 +97,7 @@ char		test_adv(char **grid)
 			if (test_win(grid, col) == 1)
 				ret = 0;
 			else
-				ret = 1;
+				ret = test_ai(grid);
 			grid[line - 1][col] = '.';
 		}
 		col++;
@@ -56,7 +119,7 @@ char		test_token(char **grid, int col, char token)
 		line++;
 	grid[line - 1][col] = token;
 	if (test_win(grid, col) == 1)
-		ret = 3;
+		ret = 10;
 	else
 		ret = test_adv(grid);
 	display(grid);
